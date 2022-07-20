ce17a8f52b867680bce9ea0c4b38090d35069cfb
Tarek <99592820+Mhdtarek@users.noreply.github.com>
2022-07-20 22:16:35 +0200

    Revert "authentication"

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,8 @@
 <script>
 import Allposts from './Components/allPosts.svelte'
 import NavBar from "./Components/navBar.svelte"
-import Auth from './Components/Auth.svelte'
-import {username, userDisplayName, userCreation, userPhotoURL, role, klass, isLoggedIn, isNotLoggedIn} from './Components/Auth.svelte'
 </script>
 <main>
-<div>
 <NavBar/>
-
-
-
-{#if $isLoggedIn}
 <Allposts/>
-{/if}
-<div style="display: none;">
-    <img src={$userPhotoURL} alt="">
-    <p>{$username}</p>
-    <p>{$userDisplayName}</p>
-    <p>{$userPhotoURL} </p>
-    <p>{$userCreation}</p>
-    <p>{$role}</p>
-    <p>{$klass}</p>
-</div>
-{#if !$isNotLoggedIn}
-    <div>  
-        <Auth/>
-    </div>
-{/if}
-{#if $role === 'teacher'}
-teacher
-{/if}
 </main>
-<style>
-
-    
-</style>
--- src/Components/navBar.svelte
+++ src/Components/navBar.svelte
@@ -5,29 +5,19 @@
       Nav,
       NavItem,
       NavLink,
-      Dropdown,
-      DropdownItem,
-      DropdownMenu,
-      DropdownToggle
     } from 'sveltestrap';
-  import {userPhotoURL, userDisplayName, userCreation, isLoggedIn} from './Auth.svelte'
+  
   </script>
+  
 <Navbar color="light" light expand="md">
-  <NavbarBrand href="#">bbrr</NavbarBrand>
-  <Nav class="ms-auto" navbar>
-    <NavItem>
-      {#if $isLoggedIn}
-      <Dropdown direction="left" nav setActiveFromChild>
-        <DropdownToggle nav class="nav-link"><img style="border-radius: .25rem;" src={$userPhotoURL} alt="user" height="32" width="32"></DropdownToggle>
-        <DropdownMenu dark>
-          <DropdownItem header>{$userDisplayName}</DropdownItem>
-            <DropdownItem href="#">inställningar</DropdownItem>
-            <DropdownItem color="primary" on:click={() => window.location.reload()}>Logga ut</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        {/if}
+  <NavbarBrand href="/">bbrr</NavbarBrand>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink>Post</NavLink>
       </NavItem>
-      <NavItem>    
+      <NavItem>
+        <NavLink>Chat</NavLink>
       </NavItem>
+
     </Nav>
 </Navbar>
