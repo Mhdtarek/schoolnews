420426e13de9fbf9a375f32fa2c4b80932c83a34
Tarek <mhdtarekalkasemi@gmail.com>
2022-10-05 18:16:11 +0200

    cool app update

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,40 @@
 <script>
-// @ts-ignore
-import Posts from './Components/Posts.svelte'
-import NavBar from "./Components/navBar.svelte"
-import Auth from './Components/Auth.svelte'
-import {username, userDisplayName, userCreation, userPhotoURL, role, klass, isLoggedIn, isNotLoggedIn} from './Components/Auth.svelte'
+  // @ts-ignore
+  import Posts from "./Components/Posts.svelte";
+  import NavBar from "./Components/navBar.svelte";
+  import Auth from "./Components/Auth.svelte";
+  import {
+    username,
+    userDisplayName,
+    userCreation,
+    userPhotoURL,
+    role,
+    klass,
+    isLoggedIn,
+    isNotLoggedIn,
+  } from "./Components/Auth.svelte";
 </script>
+
 <main>
-<div>
-<NavBar/>
-{#if $isLoggedIn}
-<Posts/>
-{/if}
-<div style="display: none;">
-    <img src={$userPhotoURL} alt="">
+  <NavBar />
+  {#if $isLoggedIn}
+    <Posts />
+  {/if}
+  <div style="display: none;">
+    <img src={$userPhotoURL} alt="" />
     <p>{$username}</p>
     <p>{$userDisplayName}</p>
-    <p>{$userPhotoURL} </p>
+    <p>{$userPhotoURL}</p>
     <p>{$userCreation}</p>
     <p>{$role}</p>
     <p>{$klass}</p>
-</div>
-{#if !$isNotLoggedIn}
-    <div>  
-        <Auth/>
+  </div>
+  {#if !$isNotLoggedIn}
+    <div>
+      <Auth />
     </div>
-{/if}
-
+  {/if}
 </main>
-<style>
 
-    
+<style>
 </style>
